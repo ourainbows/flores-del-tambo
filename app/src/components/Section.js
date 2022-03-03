9401c3179da19229d3a52d823e5f63b3575fa47e
Zizi <valeriaziegler21@gmail.com>
2022-03-03 11:09:19 -0500

    Frontend structure and comments

--- app/src/components/Section.js
+++ app/src/components/Section.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Section = ({ children }) => {
     return (
-        <div style={{ margin: "50px" }}>
+        <div style={{ margin: "10px" }}>
             {children}
         </div>
     );
