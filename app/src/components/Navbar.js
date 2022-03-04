e3b5f5f3291f71fde001193b112d82069d5248bc
Zizi <valeriaziegler21@gmail.com>
2022-03-04 10:31:19 -0500

    pull

--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -1,23 +1,31 @@
 import React from 'react';
-import { BsListCheck } from 'react-icons/bs';
+/* import { BsListCheck } from 'react-icons/bs'; */
 import { MdDelete } from 'react-icons/md';
 import "../styles/navbar.css"
-import logo from '../assets/Logoflor.png'
+import logo from '../assets/Logoflor.png';
+import Button from 'react-bootstrap/Button';
+
+
 
 const Navbar = () => {
-  return (
+
+return (
+     
     <div  className="navbar">
       <div className="nav">
       <h1>Flores del Tambo</h1>
       <div className="icons">
-      <BsListCheck />
-      <MdDelete/>
-      </div>
+     <MdDelete/>
+     </div>
       </div>
       <img src={logo} alt={logo}/>
       <p>El éxito es la suma de pequeños esfuerzos repetidos un día sí y otro también. </p>
-    </div>
+      <div className="d-grid gap-2">
+        </div>
+      </div>
+      
+
   )
-}
+};
 
 export default Navbar
