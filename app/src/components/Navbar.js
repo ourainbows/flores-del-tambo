580963a6cf26f688ae179fdae2856fdb6cd6dd45
Angie <agladinop@gmail.com>
2022-03-03 22:28:08 -0500

    bootstrap

--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import { BsListCheck } from 'react-icons/bs';
-import { MdDelete } from 'react-icons/md';
-import "../styles/navbar.css"
-import logo from '../assets/Logoflor.png'
+import React from "react";
+import { BsListCheck } from "react-icons/bs";
+import { MdDelete } from "react-icons/md";
+import "../styles/navbar.css";
+import logo from "../assets/Logoflor.png";
 
 const Navbar = () => {
   return (
-    <div  className="navbar">
+    <div className="navbar">
       <div className="nav">
-      <h1>Flores del Tambo</h1>
-      <div className="icons">
-      <BsListCheck />
-      <MdDelete/>
+        <h1>Flores del Tambo</h1>
+        <div className="icons">
+          <BsListCheck />
+          <MdDelete />
+        </div>
       </div>
-      </div>
-      <img src={logo} alt={logo}/>
-      <p>El éxito es la suma de pequeños esfuerzos repetidos un día sí y otro también. </p>
+      <img src={logo} alt={logo} />
+      <p>
+        El éxito es la suma de pequeños esfuerzos repetidos un día sí y otro
+        también.{" "}
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
