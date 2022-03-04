625eb72f152300c063950ce741bafb2863b83788
Angie <agladinop@gmail.com>
2022-03-03 19:12:09 -0500

    readme edited

--- app/src/apis/index.js
+++ app/src/apis/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3030",
+  baseURL: "http://localhost:3030", // Instance of Axios. All of the calls will simply require the URI for the HTTP calls, without the full URL.
 });
--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -1,23 +1,37 @@
-import React from 'react';
-import { BsListCheck } from 'react-icons/bs';
-import { MdDelete } from 'react-icons/md';
-import "../styles/navbar.css"
-import logo from '../assets/Logoflor.png'
-
+import React from "react";
+import { BsListCheck } from "react-icons/bs";
+import { MdDelete } from "react-icons/md";
+import "../styles/navbar.css";
+import logo from "../assets/Logoflor.png";
+import Button from "react-bootstrap/Button";
 const Navbar = () => {
   return (
-    <div  className="navbar">
-      <div className="nav">
-      <h1>Flores del Tambo</h1>
-      <div className="icons">
-      <BsListCheck />
-      <MdDelete/>
+    <>
+      54
+      <div className="navbar">
+        <div className="nav">
+          <h1>Flores del Tambo</h1>
+          <div className="icons">
+            <BsListCheck />
+            <MdDelete />
+          </div>
+        </div>
+        <img src={logo} alt={logo} />
+        <p>
+          El éxito es la suma de pequeños esfuerzos repetidos un día sí y otro
+          también.
+        </p>
+        <div className="d-grid gap-2">
+          <Button variant="primary" size="lg">
+            Block level button
+          </Button>
+          <Button variant="secondary" size="lg">
+            Block level button
+          </Button>
+        </div>
       </div>
-      </div>
-      <img src={logo} alt={logo}/>
-      <p>El éxito es la suma de pequeños esfuerzos repetidos un día sí y otro también. </p>
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
